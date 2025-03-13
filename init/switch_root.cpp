5ee30da9a99ca23ef936a7756bf3de92d726eeae
Dmitri Gomon <dgomon42@gmail.com>
2025-03-13 19:46:40 +0200

    disable sleep

--- init/switch_root.cpp
+++ init/switch_root.cpp
@@ -76,14 +76,14 @@ void SwitchRoot(const std::string& new_root) {
 
     for (const auto& mount_path : mounts) {
         LOG(INFO) << "Checkpoint 5.4.1";
-        sleep(1);
+//        sleep(1);
         LOG(INFO) << "Checkpoint 5.4.2";
         LOG(INFO) << "Moving mount at '" << mount_path << "' to '" << new_root + mount_path << "'";
         auto new_mount_path = new_root + mount_path;
         mkdir(new_mount_path.c_str(), 0755);
         LOG(INFO) << "Checkpoint 5.4.3";
         LOG(INFO) << "Created directory '" << new_mount_path << "'";
-        sleep(1);
+//        sleep(1);
         LOG(INFO) << "Checkpoint 5.4.4";
         if (mount(mount_path.c_str(), new_mount_path.c_str(), nullptr, MS_MOVE, nullptr) != 0) {
             PLOG(FATAL) << "Unable to move mount at '" << mount_path << "' to "
